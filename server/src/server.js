60637e9fcba6b1a698c9d219aa48b9d6f5aeacfc
GerryKp <gerardferre.alu@iespacomolla.es>
2023-05-31 18:27:13 +0200

    added express

---
+++ server/src/server.js
@@ -0,0 +1,15 @@
+//imports
+const http  = require('http');
+const app = require('./app');
+
+const PORT = process.envPORT || 8000;
+
+const server = http.createServer(app);
+
+
+server.listen(PORT, () =>{
+  console.log(`Server listening on port: ${PORT}`);
+});
+
+
+ 
