2f81f34a53ce137c57069b531d256c490f3cc289
GerryKp <gerardferre.alu@iespacomolla.es>
2023-06-01 19:40:03 +0200

    serve frontend via server

--- server/src/server.js
+++ server/src/server.js
@@ -1,15 +1,22 @@
 //imports
 const http  = require('http');
 const app = require('./app');
+const {loadPlanetsData} = require('./models/planets.model');
 
 const PORT = process.envPORT || 8000;
 
 const server = http.createServer(app);
 
+//loading data before listening to port
+async function startServer (){
+  await loadPlanetsData();
+  //ended loading sata
+  server.listen(PORT, () =>{
+    console.log(`Server listening on port: ${PORT}`);
+  });
+}
 
-server.listen(PORT, () =>{
-  console.log(`Server listening on port: ${PORT}`);
-});
+startServer();
 
 
  
